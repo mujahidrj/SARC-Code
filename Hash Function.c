769e5dd94240a3021f4f1b6578b0a29bf7ebc4c3
mujahidj <43118863+mujahidrj@users.noreply.github.com>
2019-09-15 22:35:04 -0400

    Second commit

---
+++ Hash Function.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "string.h"
+
+int hash(int number)
+{
+  return number % 10;
+}
+
+int main(void) {
+
+	int x;
+
+  hash(12);
+  hash(5809);
+  hash(80);
+  hash(43);
+  hash(52);
+
+  return 0;
+}
